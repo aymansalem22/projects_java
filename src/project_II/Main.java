35144e445f479b83518c4edf80920098d4f9e957
aymansalem22 <s.aymansalem@gmail.com>
2023-05-04 13:34:36 +0200

    Task II: Display the game board

--- src/project_II/Main.java
+++ src/project_II/Main.java
@@ -5,12 +5,40 @@ package project_II;
 public class Main {
 
 	public static void main(String[] args) {
-		//create a 3x3 character array that represents our tic tac toe board
+		////Let's create a 3x3 character array that represents our tic tac toe board
 		char[][] game_board=new char[3][3];
 		
+		//variables to keep track of players
+		char c='-';
+		boolean p1Turn=true;
 		
+		//Initialize our board with '-'
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				game_board[i][j]='-';
+			}
+		}
+		
+		displayBoard(game_board);
+		
+		System.out.println("\nThe old value of c = '"+c+"'");//just for test
+	
 	}
 	
+	public static void displayBoard(char[][] game_board) {
+		System.out.println("\nTic Tac Toe Board");
+		
+		//Outer loop will follow the row-major order
+		for(int i=0;i<3;i++) {
+			//Inner loop will follow the column-major order
+			for(int j=0;j<3;j++) {
+				System.out.println(game_board[i][j]+" ");
+			}
+			//This will make a new line after every row
+			System.out.println();
+		}
+	}
 	
+    
 
 }
