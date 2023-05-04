a03b8a6f41bd46dad8f1a7fd6e187c12bd70dacf
aymansalem22 <s.aymansalem@gmail.com>
2023-05-04 13:32:14 +0200

    Task I: Create a Tic-tac-toe board

---
+++ src/project_II/Main.java
@@ -0,0 +1,16 @@
+package project_II;
+
+
+//Project II: Tic-Tac-Toe
+public class Main {
+
+	public static void main(String[] args) {
+		//create a 3x3 character array that represents our tic tac toe board
+		char[][] game_board=new char[3][3];
+		
+		
+	}
+	
+	
+
+}
