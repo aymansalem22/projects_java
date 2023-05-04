827c81798c10abb583c6bb8c22d1ff7ae0ffe626
aymansalem22 <s.aymansalem@gmail.com>
2023-05-04 13:36:47 +0200

    Task III: Identify the player’s turn

--- src/project_II/Main.java
+++ src/project_II/Main.java
@@ -1,42 +1,55 @@
 package project_II;
 
-
 //Project II: Tic-Tac-Toe
 public class Main {
 
 	public static void main(String[] args) {
-		////Let's create a 3x3 character array that represents our tic tac toe board
-		char[][] game_board=new char[3][3];
-		
-	
-		
-		//Initialize our board with '-'
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
-				game_board[i][j]='-';
+		//// Let's create a 3x3 character array that represents our tic tac toe board
+		char[][] game_board = new char[3][3];
+
+		// variables to keep track of players
+		char c = '-';
+		boolean p1Turn = true;
+
+		// Initialize our board with '-'
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				game_board[i][j] = '-';
 			}
 		}
-		
+
 		displayBoard(game_board);
-		
-		
-	
+
+		System.out.println("\nThe old value of c = '" + c + "'");// just for testing
+		c = checkTurn(c, p1Turn);
+		System.out.println("The new value of c = " + c + "'");// just for testing
+
 	}
-	
+
 	public static void displayBoard(char[][] game_board) {
 		System.out.println("\nTic Tac Toe Board");
-		
-		//Outer loop will follow the row-major order
-		for(int i=0;i<3;i++) {
-			//Inner loop will follow the column-major order
-			for(int j=0;j<3;j++) {
-				System.out.println(game_board[i][j]+" ");
+
+		// Outer loop will follow the row-major order
+		for (int i = 0; i < 3; i++) {
+			// Inner loop will follow the column-major order
+			for (int j = 0; j < 3; j++) {
+				System.out.println(game_board[i][j] + " ");
 			}
-			//This will make a new line after every row
+			// This will make a new line after every row
 			System.out.println();
 		}
 	}
-	
-    
+
+	public static char checkTurn(char c, boolean p1Turn) {
+
+		if (p1Turn) {
+			System.out.println("Player1's Turn (x):");
+			c = 'x';
+		} else {
+			System.out.println("Player2's Turn (o):");
+			c = 'o';
+		}
+		return c;
+	}
 
 }
